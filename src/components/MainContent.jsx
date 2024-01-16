c573d990925571ea0af022423b67aefeacb09b6a
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-15 17:40:07 -0700

    Add add button click state and lift it to MainContent component

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,27 +1,38 @@
+import { useState } from "react";
 import Sidebar from "./Sidebar";
+import NewProjectForm from "./NewProjectForm";
 
 export default function MainContent() {
+  const [isAddBtnClicked, setIsAddBtnClicked] = useState(false);
+
+  function handleClick() {
+    setIsAddBtnClicked(true);
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <div className="w-[35rem] mt-16">
-        <img
-          className="w-16 h-16 object-contain mx-auto"
-          src="logo.png"
-          alt="An image of pad and pen"
-        />
-        <h2 className="text-xl font-bold text-stone-700 my-4">
-          No project Selected
-        </h2>
-        <p className="text-stone-600 mb-4">
-          Select a project or get started with a new one
-        </p>
-        <form action="">
+      <Sidebar addBtnClick={handleClick} />
+      {!isAddBtnClicked ? (
+        <div className="w-[35rem] mt-16">
+          <img
+            className="w-16 h-16 object-contain mx-auto"
+            src="logo.png"
+            alt="An image of pad and pen"
+          />
+          <h2 className="text-xl font-bold text-stone-700 my-4">
+            No project Selected
+          </h2>
+          <p className="text-stone-600 mb-4">
+            Select a project or get started with a new one
+          </p>
+
           <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
             Create new project
           </button>
-        </form>
-      </div>
+        </div>
+      ) : (
+        <NewProjectForm />
+      )}
     </main>
   );
 }
