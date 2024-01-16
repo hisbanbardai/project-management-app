46e3b7bd5048570ad3d5a449d8b6769fb2dc4cd9
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-15 17:44:30 -0700

    Add styles to NewProjectForm component

---
+++ src/components/NewProjectForm.jsx
@@ -0,0 +1,33 @@
+export default function NewProjectForm() {
+  return (
+    <form className="mt-4 text-right" action="">
+      <div>
+        <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+        <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          Save
+        </button>
+      </div>
+      <label className="text-sm font-bold uppercase text-stone-500">
+        TITLE
+        <input
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+          type="text"
+        />
+      </label>
+      <label className="text-sm font-bold uppercase text-stone-500">
+        DESCRIPTION
+        <input
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+          type="text"
+        />
+      </label>
+      <label className="text-sm font-bold uppercase text-stone-500">
+        DUE DATE
+        <input
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+          type="date"
+        />
+      </label>
+    </form>
+  );
+}
