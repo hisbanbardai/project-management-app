7a783ff9142c611fe48f510f7907ed07a96b6407
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-17 12:10:23 -0700

    Add conditional rendering to show NewForm and ProjectDetail components

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -39,7 +39,10 @@ export default function MainContent() {
   }
 
   function handleAddBtnClick(val) {
-    val ? setIsAddBtnClicked(true) : setIsAddBtnClicked(false);
+    if (val) {
+      setIsProjectClicked(false);
+      setIsAddBtnClicked(true);
+    } else setIsAddBtnClicked(false);
   }
 
   function handleProjectTitleClick() {
@@ -49,8 +52,12 @@ export default function MainContent() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar addBtnClick={handleAddBtnClick} projectsList={projects} />
-      {!isAddBtnClicked ? (
+      <Sidebar
+        addBtnClick={handleAddBtnClick}
+        projectsList={projects}
+        projectTitleClick={handleProjectTitleClick}
+      />
+      {/* {!isAddBtnClicked ? (
         <div className="w-[35rem] mt-16">
           <img
             className="w-16 h-16 object-contain mx-auto"
@@ -72,6 +79,39 @@ export default function MainContent() {
           </button>
         </div>
       ) : (
+        <NewProjectForm
+          ref={projectDetails}
+          onClick={handleClickSubmit}
+          addBtnClick={handleAddBtnClick}
+        />
+      )}
+
+      {isProjectClicked && <ProjectDetail />} */}
+
+      {!isAddBtnClicked && !isProjectClicked && (
+        <div className="w-[35rem] mt-16">
+          <img
+            className="w-16 h-16 object-contain mx-auto"
+            src="logo.png"
+            alt="An image of pad and pen"
+          />
+          <h2 className="text-xl font-bold text-stone-700 my-4">
+            No project Selected
+          </h2>
+          <p className="text-stone-600 mb-4">
+            Select a project or get started with a new one
+          </p>
+
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={() => handleAddBtnClick(true)}
+          >
+            Create new project
+          </button>
+        </div>
+      )}
+
+      {isAddBtnClicked && (
         <NewProjectForm
           ref={projectDetails}
           onClick={handleClickSubmit}
