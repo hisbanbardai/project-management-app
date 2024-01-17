db3235fbf09bab6151119d9dbdec2ceb9c6270d6
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-17 12:09:38 -0700

    Pass projectTitleClick function as a prop to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
-export default function Sidebar({ addBtnClick, projectsList }) {
+export default function Sidebar({
+  addBtnClick,
+  projectsList,
+  projectTitleClick,
+}) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -16,7 +20,9 @@ export default function Sidebar({ addBtnClick, projectsList }) {
       <ul>
         {projectsList.length > 0 &&
           projectsList.map((project) => (
-            <li key={project.title}>{project.title}</li>
+            <li key={project.title} onClick={projectTitleClick}>
+              {project.title}{" "}
+            </li>
           ))}
       </ul>
     </aside>
