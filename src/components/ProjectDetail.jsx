b4cc53ceb57c6c514c6ecd056aaf8154b058af51
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-17 11:37:03 -0700

    Add ProjectDetail component

---
+++ src/components/ProjectDetail.jsx
@@ -0,0 +1,10 @@
+export default function ProjectDetail() {
+  return (
+    <>
+      <h1>Project Title</h1>
+      <button>Delete</button>
+      <p>Due Date</p>
+      <p>Project Description</p>
+    </>
+  );
+}
