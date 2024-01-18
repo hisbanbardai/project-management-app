0d7ba5fd0f48be8c679cd57337eb19a574c59599
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-18 13:44:14 -0700

    Add a taskName ref and connect it to the input element

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+const tasks = [];
 
 export default function Tasks() {
   const [taskExist, setTaskExist] = useState(false);
+  const taskName = useRef();
 
   return (
     <>
       <h2>Tasks</h2>
       <input type="text" />
-      <button>Add Task</button>
+      <button ref={taskName}>Add Task</button>
       {!taskExist && <p>This project does not have any tasks yet</p>}
     </>
   );
