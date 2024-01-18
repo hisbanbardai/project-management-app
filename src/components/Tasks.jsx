12b54f1bfae4e5e4081a2a4487353d87c9e6d6d5
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-18 14:34:20 -0700

    Add a tasks state to manage the list of tasks and pass it as a prop to Task component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,15 +1,15 @@
 import { useRef, useState } from "react";
-
-const tasks = [];
+import Task from "./Task";
 
 export default function Tasks() {
+  const [tasks, setTasks] = useState([]);
   const [taskExist, setTaskExist] = useState(false);
   const taskName = useRef();
 
   function handleAddTaskBtnClick() {
     setTaskExist(true);
     const task = taskName.current.value;
-    tasks.push(task);
+    setTasks((prevTasks) => [...prevTasks, task]);
     taskName.current.value = "";
   }
 
@@ -19,6 +19,7 @@ export default function Tasks() {
       <input ref={taskName} type="text" />
       <button onClick={handleAddTaskBtnClick}>Add Task</button>
       {!taskExist && <p>This project does not have any tasks yet</p>}
+      {taskExist && <Task tasksList={tasks} />}
     </>
   );
 }
