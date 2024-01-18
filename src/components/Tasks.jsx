3259fac19834ad8968c61ea033770051f0389b90
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-18 13:56:54 -0700

    Add a handleAddTaskBtnClick function to add task into tasks array and set taskExist state to true

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,11 +6,18 @@ export default function Tasks() {
   const [taskExist, setTaskExist] = useState(false);
   const taskName = useRef();
 
+  function handleAddTaskBtnClick() {
+    setTaskExist(true);
+    const task = taskName.current.value;
+    tasks.push(task);
+    taskName.current.value = "";
+  }
+
   return (
     <>
       <h2>Tasks</h2>
-      <input type="text" />
-      <button ref={taskName}>Add Task</button>
+      <input ref={taskName} type="text" />
+      <button onClick={handleAddTaskBtnClick}>Add Task</button>
       {!taskExist && <p>This project does not have any tasks yet</p>}
     </>
   );
