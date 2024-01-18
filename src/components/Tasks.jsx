536094f596c63ce923708c7b209e8014de6a949e
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-18 13:34:00 -0700

    Add a Tasks component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,10 @@
+export default function Tasks() {
+  return (
+    <>
+      <h2>Tasks</h2>
+      <input type="text" />
+      <button>Add Task</button>
+      <p>This project does not have any tasks yet</p>
+    </>
+  );
+}
