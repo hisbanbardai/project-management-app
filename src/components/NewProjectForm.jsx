5f14b7c146673642776edbfc6036a38cf0a01be0
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-20 16:41:54 -0700

    Update html structure of NewProjectForm component

--- src/components/NewProjectForm.jsx
+++ src/components/NewProjectForm.jsx
@@ -20,46 +20,58 @@ const NewProjectForm = forwardRef(function ({ onClick, addBtnClick }, ref) {
   });
 
   return (
-    <>
+    <div>
+      <menu>
+        <li>
+          <button
+            className="text-stone-800 hover:text-stone-950"
+            onClick={() => addBtnClick(false)}
+          >
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={onClick}
+          >
+            Save
+          </button>
+        </li>
+      </menu>
       <div>
-        <button
-          className="text-stone-800 hover:text-stone-950"
-          onClick={() => addBtnClick(false)}
-        >
-          Cancel
-        </button>
-        <button
-          className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          onClick={onClick}
-        >
-          Save
-        </button>
+        <p>
+          <label className="text-sm font-bold uppercase text-stone-500">
+            TITLE
+          </label>
+          <input
+            className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+            type="text"
+            ref={projectTitle}
+          />
+        </p>
+        <p>
+          <label className="text-sm font-bold uppercase text-stone-500">
+            DESCRIPTION
+          </label>
+          <input
+            className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+            type="text"
+            ref={projectDescription}
+          />
+        </p>
+        <p>
+          <label className="text-sm font-bold uppercase text-stone-500">
+            DUE DATE
+          </label>
+          <input
+            className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+            type="date"
+            ref={projectDueDate}
+          />
+        </p>
       </div>
-      <label className="text-sm font-bold uppercase text-stone-500">
-        TITLE
-        <input
-          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-          type="text"
-          ref={projectTitle}
-        />
-      </label>
-      <label className="text-sm font-bold uppercase text-stone-500">
-        DESCRIPTION
-        <input
-          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-          type="text"
-          ref={projectDescription}
-        />
-      </label>
-      <label className="text-sm font-bold uppercase text-stone-500">
-        DUE DATE
-        <input
-          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-          type="date"
-          ref={projectDueDate}
-        />
-      </label>
-    </>
+    </div>
   );
 });
 
