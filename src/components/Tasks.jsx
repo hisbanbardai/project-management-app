33e4719d757c96f0649a688ed1bbe393cdf719cf
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-21 16:04:05 -0700

    Update css styles of Tasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -4,11 +4,16 @@ import Task from "./Task";
 export default function Tasks({ tasksList, addTaskBtn, deleteTaskBtn }) {
   const taskName = useRef();
   return (
-    <>
-      <h2>Tasks</h2>
-      <div>
-        <input ref={taskName} type="text" />
+    <section>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      <div className="flex items-center gap-4">
+        <input
+          ref={taskName}
+          type="text"
+          className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        />
         <button
+          className="text-stone-700 hover:text-stone-950"
           onClick={() => {
             addTaskBtn(taskName.current.value);
             taskName.current.value = "";
@@ -18,11 +23,15 @@ export default function Tasks({ tasksList, addTaskBtn, deleteTaskBtn }) {
         </button>
       </div>
       <div>
-        {!tasksList.length && <p>This project does not have any tasks yet</p>}
+        {!tasksList.length && (
+          <p className="text-stone-800 my-4">
+            This project does not have any tasks yet
+          </p>
+        )}
         {tasksList.length > 0 && (
           <Task tasksList={tasksList} deleteTask={deleteTaskBtn} />
         )}
       </div>
-    </>
+    </section>
   );
 }
