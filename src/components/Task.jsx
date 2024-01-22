51153c176bf797405bd3bcba3e5acded89619016
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-22 14:43:45 -0700

    Update css styles of Task component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,16 @@
 export default function Task({ tasksList, deleteTask }) {
   return (
-    <ul>
+    <ul className="p-4 mt-8 rounded-md bg-stone-100">
       {tasksList.map((task) => {
         return (
-          <li key={task}>
+          <li key={task} className="flex justify-between my-4">
             <span>{task}</span>
-            <button onClick={() => deleteTask(task)}>Clear</button>
+            <button
+              className="text-stone-700 hover:text-red-500"
+              onClick={() => deleteTask(task)}
+            >
+              Clear
+            </button>
           </li>
         );
       })}
